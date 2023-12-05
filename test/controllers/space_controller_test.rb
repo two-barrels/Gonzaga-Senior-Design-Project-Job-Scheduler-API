6c0330e8ddf74e68c7a9bdb9a3d6a42c6aef1634
EFigueroa12 <efigueroa6912@gmail.com>
2023-12-04 17:28:59 -0800

    plez

--- test/controllers/space_controller_test.rb
+++ test/controllers/space_controller_test.rb
@@ -6,13 +6,13 @@ class SpaceControllerTest < ActionDispatch::IntegrationTest
   end
 
   test 'should create space' do
-    post /spaces, params: { spaces_name: 'Room', floor_id: 1, max_occupancy: 1, description: "Description"}, as: :json
+    post spaces, params: { spaces_name: 'Room', floor_id: 1, max_occupancy: 1, description: "Description"}, as: :json
 
     assert_response :created
   end
 
   test 'should destroy space' do
-    delete /spaces/@Space1.id, as: :json
+    delete spaces, @Space1.id, as: :json
 
     assert_response :deleted
   end

364797497cecfc379de7f3973a77c43a08547101
drange <darianvaughnrange@gmail.com>
2024-02-06 14:20:28 -0800

    put the wrong variable name for member data

--- app/controllers/members_controller.rb
+++ app/controllers/members_controller.rb
@@ -3,7 +3,7 @@
 class MembersController < ApplicationController
 
   def show
-    user = current_user
+    user = @current_user
     render json: {
       message: "If you see this, you're in!",
       user:

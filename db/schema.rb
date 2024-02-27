688c579a4b97dcfe42a9373fa561964743abf4e6
drange <darianvaughnrange@gmail.com>
2024-02-27 09:41:07 -0800

    bug fix

--- db/schema.rb
+++ db/schema.rb
@@ -1,4 +1,17 @@
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# This file is the source Rails uses to define your schema when running `bin/rails
+# db:schema:load`. When creating a new database, `bin/rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
+#
+# It's strongly recommended that you check this file into your version control system.
+
 ActiveRecord::Schema[7.0].define(version: 2024_02_17_232940) do
+  # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
   enable_extension "postgis"
 
@@ -45,34 +58,3 @@ ActiveRecord::Schema[7.0].define(version: 2024_02_17_232940) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "space_geometries", force: :cascade do |t|
-    t.bigint "space_id", null: false
-    t.geometry "shape", limit: {:srid=>0, :type=>"geometry"}
-    t.index ["space_id"], name: "index_space_geometries_on_space_id"
-  end
-
-  create_table "spaces", force: :cascade do |t|
-    t.string "spaces_name"
-    t.integer "floor_id"
-    t.integer "max_occupancy"
-    t.text "description"
-  end
-
-  create_table "users", force: :cascade do |t|
-    t.string "name"
-    t.string "relationship"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.string "email", default: "", null: false
-    t.string "encrypted_password", default: "", null: false
-    t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.index ["email"], name: "index_users_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
-  end
-
-  add_foreign_key "assignments", "roles"
-  add_foreign_key "assignments", "users"
-  add_foreign_key "space_geometries", "spaces"
-end

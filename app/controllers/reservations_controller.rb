bec04c5226895018cddecd8e48ada60546f75a7f
drange <darianvaughnrange@gmail.com>
2024-03-27 16:51:11 -0700

    hotfix for reservation create

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -31,9 +31,9 @@ class ReservationsController < ApplicationController
   def create
     reservation = reservation_params
     reservation[:user_id] = @current_user.id
-    @Reservation = Reservation.new(reservation_params)
+    puts "DR TEST " + @current_user.to_s
 
-    if @Reservation.save
+    if @Reservation = Reservation.create!(reservation)
       render json: @Reservation, status: :created, location: @Reservation
     else
       render json: @Reservation.errors, status: :unprocessable_entity

a3d62affe40406f80f59692c2d6c44fd9581800e
drange <darianvaughnrange@gmail.com>
2024-04-09 22:03:29 -0700

    please

--- app/models/building.rb
+++ app/models/building.rb
@@ -1,3 +1,3 @@
 class Building < ApplicationRecord
-    has_many :floors, optional: true
+    has_many :floors
 end
--- app/models/floor.rb
+++ app/models/floor.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
 class Floor < ApplicationRecord
-  belongs_to :building, :dependent => :destroy
+  belongs_to :building, optional: true
   has_many :spaces
 end

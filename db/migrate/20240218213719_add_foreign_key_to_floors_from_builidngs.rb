9d3f4e5976a3c86952088c88b91e689d1016dcae
drange <darianvaughnrange@gmail.com>
2024-04-09 21:43:29 -0700

    I think I got it and I fixed the migrations

--- db/migrate/20240218213719_add_foreign_key_to_floors_from_builidngs.rb
+++ db/migrate/20240218213719_add_foreign_key_to_floors_from_builidngs.rb
@@ -1,6 +1,6 @@
 class AddForeignKeyToFloorsFromBuilidngs < ActiveRecord::Migration[7.0]
   def change
-    add_column :buildings, :name, :string
+    add_column :floors, :building_id, :integer
     add_foreign_key :floors, :buildings
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -1,6 +1,16 @@
-# frozen_string_literal: true
-ActiveRecord::Schema[7.0].define(version: 2024_04_06_191819) do
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# This file is the source Rails uses to define your schema when running `bin/rails
+# db:schema:load`. When creating a new database, `bin/rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
+#
+# It's strongly recommended that you check this file into your version control system.
 
+ActiveRecord::Schema[7.0].define(version: 2024_04_06_191819) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
   enable_extension "postgis"
@@ -13,10 +23,11 @@ ActiveRecord::Schema[7.0].define(version: 2024_04_06_191819) do
     t.index ["role_id"], name: "index_assignments_on_role_id"
     t.index ["user_id"], name: "index_assignments_on_user_id"
   end
+
   create_table "buildings", force: :cascade do |t|
+    t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "name"
   end
 
   create_table "floors", force: :cascade do |t|
